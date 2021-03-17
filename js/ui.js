c3f0e3d2546db78c28cda2b88d92da675c71b01a
bbig3831 <beb601@mail.harvard.edu>
2021-03-16 22:39:37 -0400

    Add more representative data to list

--- js/ui.js
+++ js/ui.js
@@ -12,11 +12,24 @@ class UI {
     let output = '<ul class="collection">';
 
     reps.forEach(rep => {
+      // Get social media data
+      let facebookId = rep.channels.find(x => x.type === 'Facebook');
+      let twitterId = rep.channels.find(x => x.type === 'Twitter');
+
       output += `
         <li class="collection-item avatar">
           <img src="${rep.photoUrl}" alt="Photo of ${rep.name}" class="circle large">
-          <span class="title">${rep.name}</span>
+          <span class="title">${rep.name} (${rep.party})</span>
+          <p>${rep.address[0].line1}</p>
+          <p>${rep.address[0].city}, ${rep.address[0].state} ${rep.address[0].zip}</p>
+
+          <span>
+          <a href="${facebookId}" class="secondary-content"><i class="material-icons">facebook</i></a>
+
+          <a href="${twitterId}" class="secondary-content"><i class="material-icons">twitter</i></a>
+
           <a href="${rep.urls[0]}" class="secondary-content"><i class="material-icons">public</i></a>
+          </span>
         </li>
       `
     });
